d3631f1cfe4ef75766ca560ddfa90dcc7a89d23e
淡暗 <squallmouse@gmail.com>
2025-02-02 22:58:09 +0800

    我的订单

--- apps/orders/urls.py
+++ apps/orders/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     re_path("^orders/settlement/$", views.OrderSettlementView.as_view(), name="settlement"),
     re_path("^orders/commit/$",views.OrderCommitView.as_view()),
     re_path("^orders/success/$",views.OrderSuccessView.as_view(),name="success"),
+    re_path("^orders/info/(?P<page_num>\\d+)/$",views.UserOrderInfoView.as_view(),name="info"),
 ]
--- apps/orders/views.py
+++ apps/orders/views.py
@@ -4,6 +4,7 @@ from decimal import Decimal
 
 from django import http
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.paginator import Paginator, EmptyPage
 from django.shortcuts import render
 from django.views import View
 from django_redis import get_redis_connection
@@ -210,3 +211,43 @@ class OrderSuccessView(LoginRequiredMixin, View):
         }
 
         return render(request, "order_success.html", context)
+
+class UserOrderInfoView(LoginRequiredMixin,View):
+    """我的订单"""
+    @staticmethod
+    def get(request,page_num):
+        """提供我的订单页面"""
+        user = request.user
+        # 查询订单
+        orders = user.orderinfo_set.all().order_by("-create_time")
+        # 遍历所有订单
+        for order in orders:
+            # 绑定订单状态
+            order.status_name = OrderInfo.ORDER_STATUS_CHOICES[order.status - 1][1]
+            # 绑定支付方式
+            order.pay_method_name = OrderInfo.PAY_METHOD_CHOICES[order.pay_method - 1][1]
+            order.sku_list = []
+            # 查询订单商品
+            order_goods = order.skus.all()
+            # 遍历订单商品
+            for order_good in order_goods:
+                sku = order_good.sku
+                sku.count = order_good.count
+                sku.amount = sku.price * sku.count
+                order.sku_list.append(sku)
+
+        # 分页
+        page_num = int(page_num)
+        try:
+            paginator = Paginator(orders, 5)
+            page_orders = paginator.page(page_num)
+            total_page = paginator.num_pages
+        except EmptyPage:
+            return http.HttpResponseNotFound('订单不存在')
+
+        context = {
+            "page_orders": page_orders,
+            'total_page' : total_page,
+            'page_num'   : page_num,
+        }
+        return render(request, "user_center_order.html", context)
